51a43103bf3a83f16813f5858c9b4ed5289ff827
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-21 18:29:59 +0100

    aufgabe 4

---
+++ src/Aufgabe4_1810653996.java
@@ -0,0 +1,46 @@
+public class Aufgabe4_1810653996 {
+
+    public static void main(String[] args) {
+
+        int zahl1 = 5;
+        int zahl2 = 10;
+
+        //zahl1 kleiner zahl2
+        System.out.println(zahl1<zahl2);
+
+        //zahl2 kleiner zahl1
+        System.out.println(zahl2<zahl1);
+
+        //zahl1 größer zahl2
+        System.out.println(zahl1>zahl2);
+
+        //zahl2 größer zahl1
+        System.out.println(zahl2>zahl1);
+
+        //zahl1 kleiner oder gleich zahl2
+        System.out.println(zahl1<=zahl2);
+
+        //zahl2 kleiner oder gleich zahl1
+        System.out.println(zahl2<=zahl1);
+
+        //zahl1 größer oder gleich zahl2
+        System.out.println(zahl1>=zahl2);
+
+        //zahl2 größer oder gleich zahl1
+        System.out.println(zahl2>=zahl1);
+
+        //zahl1 gleich zahl2 (Vergleich, keine Zuweisung!)
+        System.out.println(zahl1==zahl2);
+
+        //zahl1 ungleich zahl2
+        System.out.println(zahl1!=zahl2);
+
+
+
+
+
+
+
+
+    }
+}
