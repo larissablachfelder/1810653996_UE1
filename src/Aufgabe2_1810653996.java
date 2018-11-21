f04c204184120b742ee1865b11921806d12e04f1
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-11-21 17:55:14 +0100

    aufgabe2 richtig

---
+++ src/Aufgabe2_1810653996.java
@@ -0,0 +1,23 @@
+public class Aufgabe2_1810653996 {
+
+        public static void main(String[] args) {
+
+            //übung2
+
+            String wort = "Hallo"; //Wörter
+            int zahl = 45; //Ganzzahlen 4 Byte
+            double kommazahl = 4.5; //Kommazahlen, doppelte Genauigkeit
+            boolean wahrheitswert = false; //wahrheitswert
+            long ganzzahl1 = 33;//8 byte
+            char zeichen = "??"; //zeichen
+            byte ganzzahl2 = 88; //größe 1 byte
+            short ganzzahl3 = 3; //größe 2 byte
+            float kommazahl2 = 4.6; //größe 8 byte, nicht so genau
+
+            //Variablen sind veränderbar und müssen deshalb public sein, Konstanten können nicht verändert werden
+
+            final int konstante = 218;
+
+        }
+
+}
---
+++ src/Aufgabe3_1810653996.java
@@ -0,0 +1,21 @@
+public class Aufgabe3_1810653996 {
+
+    public static void main(String[] args) {
+
+        int zahl1 = 1;
+        int zahl2 = 5;
+        int zahl3 = 7;
+
+        double zahl4 = 20.5;
+        double zahl5 = 30.5;
+        double zahl6 = 40.5;
+
+        //zahl1 plus zahl2
+        double sum1 = zahl1 + zahl2;
+        System.out.println(sum);
+
+    }
+
+
+
+}
